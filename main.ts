380ddde490976db6cea101f19f5431617ea0bf50
Muhammad Affan <164652867+paradoxaff@users.noreply.github.com>
2024-05-19 16:20:23 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,19 @@
+import inquirer from "inquirer"
+
+const answers:{
+    sentence:string
+} = await inquirer.prompt([
+    {
+        name:"sentence",
+        type:"input",
+        message:"Enter your sentence to count the word:"
+
+    }
+])
+       const words = answers.sentence.trim( ).split(" ")
+
+     //Print the array of words to the console
+       console.log(words)
+
+     //Print the word count of the sentence to the console
+       console.log(`your sentence word count is ${words.length}`);
